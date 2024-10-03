8dbc1dfc8fe88df5e07ac2aa6465fbff56f64a0a
yuyang <ongyuyang2505@gmail.com>
2024-10-03 10:12:41 -0400

    completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,43 +1,29 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
-import { useState } from 'react';
-import './CartItem.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeItem, increaseQuantity, decreaseQuantity } from "./CartSlice";
+import "./CartItem.css";
 
 const CartItem = ({ onContinueShopping }) => {
-  const cart = useSelector(state => state.cart.items);
+  const cart = useSelector((state) => state.cart.items);
   const dispatch = useDispatch();
-
-  // State for total cart quantity
-  const [totalQuantity, setTotalQuantity] = useState(0);
-
-  // Calculate total quantity and total amount when the cart changes
-  useEffect(() => {
-    const totalQty = cart.reduce((acc, item) => acc + item.quantity, 0);
-    setTotalQuantity(totalQty);
-  }, [cart]);
-
-
+  
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = (cart) => {
-    let totalCost = 0;
-    cart.forEach((item) => {
-        totalCost += item.cost * item.quantity;
+  const calculateTotalAmount = () => {
+    let total = 0;
+    cart.forEach(item => {
+     total += parseInt(item.cost.replace('$','')) * item.quantity
     });
+    return total;
   };
 
-  const handleContinueShopping = (e) => {
-    onContinueShopping();
-  };
-
-
+  const handleContinueShopping = (e) => {};
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    dispatch(increaseQuantity(item));
   };
 
   const handleDecrement = (item) => {
-   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    dispatch(decreaseQuantity(item))
   };
 
   const handleRemove = (item) => {
@@ -46,40 +32,73 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-    return(item.cost * item.quantity);
+    return  parseInt(item.cost.replace('$','')) * item.quantity;
+  };
+
+  
+
+  const handleCheckoutShopping = (e) => {
+    alert("Functionality to be added for future reference");
   };
 
   return (
     <div className="cart-container">
-      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <h2 style={{ color: "black" }}>
+        Total Cart Amount: ${calculateTotalAmount()}
+      </h2>
       <div>
-        {cart.map(item => (
+        {cart.map((item) => (
           <div className="cart-item" key={item.name}>
             <img className="cart-item-image" src={item.image} alt={item.name} />
             <div className="cart-item-details">
               <div className="cart-item-name">{item.name}</div>
               <div className="cart-item-cost">{item.cost}</div>
               <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
-                <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                <button
+                  className="cart-item-button cart-item-button-dec"
+                  onClick={() => handleDecrement(item)}
+                >
+                  -
+                </button>
+                <span className="cart-item-quantity-value">
+                  {item.quantity}
+                </span>
+                <button
+                  className="cart-item-button cart-item-button-inc"
+                  onClick={() => handleIncrement(item)}
+                >
+                  +
+                </button>
               </div>
-              <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
-              <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
+              <div className="cart-item-total">
+                Total: ${calculateTotalCost(item)}
+              </div>
+              <button
+                className="cart-item-delete"
+                onClick={() => handleRemove(item)}
+              >
+                Delete
+              </button>
             </div>
           </div>
         ))}
       </div>
-      <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
+      <div
+        style={{ marginTop: "20px", color: "black" }}
+        className="total_cart_amount"
+      ></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button
+          className="get-started-button"
+          onClick={(e) => onContinueShopping(e)}
+        >
+          Continue Shopping
+        </button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,33 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-        const {name, image, cost} = action.payload;
-        const exisitingItem = state.items.find(item => item.name === name);
-        if(exisitingItem){
-            exisitingItem.quantity++;
-        }else{
-            state.items.push({name, image, cost, quantity: 1});
-        }
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        return;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+      const { name, cost, image, quantity} = action.payload;
+      state.items.pop({ name, cost, image, quantity});
+
     },
-    updateQuantity: (state, action) => {
-        const {name, quantity} = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if(itemToUpdate){
-            itemToUpdate.quantity = quantity;
-        }
+
+    decreaseQuantity: (state, action) => {
+      const { name } = action.payload;
+      const itemToRemove = state.items.find((item) => item.name === name);
+      if (itemToRemove.quantity === 1) {
+        state.items = state.items.filter((item) => item.name !== name);
+      }
+
+      itemToRemove.quantity--;
+    },
+
+    increaseQuantity: (state, action) => {
+      const { name } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity++;
+      }
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+export const { addItem, removeItem, decreaseQuantity, increaseQuantity } =
+  CartSlice.actions;
 
 export default CartSlice.reducer;
